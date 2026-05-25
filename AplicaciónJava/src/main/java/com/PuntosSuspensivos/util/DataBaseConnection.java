211307c3aec594d1e00886649879b5a8e0ef6a1c
Tek-H <carloscorchadobenito@gmail.com>
2026-05-25 13:06:57 +0200

    Añadiendo múltiples conexiones

--- AplicaciónJava/src/main/java/com/PuntosSuspensivos/util/DataBaseConnection.java
+++ AplicaciónJava/src/main/java/com/PuntosSuspensivos/util/DataBaseConnection.java
@@ -1,21 +1,55 @@
 package com.PuntosSuspensivos.Util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DataBaseConnection {
-    private static String url1 = "jdbc:oracle:thin:@//localhost:1521/XEPDB1";
-    private static String url2 = "jdbc:oracle:thin:@//localhost:3307/XEPDB1";
-    private static String user = "user1";
-    private static String password = "1234";
+    private static final String oracleUrl = "jdbc:oracle:thin:@//localhost:1521/XEPDB1";
+
+    private static final String mysqlUrl = "jdbc:mysql://localhost:3306/PuntosSuspensivos";
+
+    private static final String userOracle = "user1";
+    private static final String userSql = "root";
+    private static final String password = "1234";
 
     private DataBaseConnection() {}
 
-
-    public static Connection getConnection() throws SQLException {
+    public static Connection getOracleConnection() throws SQLException {
         try {
-            return DriverManager.getConnection(url1, user, password);
-        }catch(SQLException ex){
-            return DriverManager.getConnection(url2, user, password);
+            Class.forName("oracle.jdbc.OracleDriver");
+            return DriverManager.getConnection(oracleUrl, userOracle, password);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver Oracle no encontrado", e);
         }
+    }
+
+    public static Connection getMySQLConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(mysqlUrl, userSql, password);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL no encontrado", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException{
+
+        try {
+            return getMySQLConnection();
+        } catch (SQLException ignore) {
+            System.out.println("No funciona MySQL");
+            ignore.printStackTrace();
+        }
+
+        try {
+            return getOracleConnection();
+        } catch (SQLException ignore) {
+            System.out.println("No funciona Oracle");
+            ignore.printStackTrace();
+        }
+
+        System.out.println("MySQL driver loaded");
+        throw new SQLException("No se pudo conectar ni a MySQL ni a Oracle");
     }
 }
